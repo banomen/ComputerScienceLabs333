878a4c93771f875296f3b3f8f053cddd987ff616
banomen <145157445+banomen@users.noreply.github.com>
2024-02-28 19:58:19 +0300

    add while loop

--- lab4/allTasks.cpp
+++ lab4/allTasks.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Omen on 26.02.2024.
-//
 #include <iostream>
 #include <cmath>
 
@@ -47,79 +44,81 @@ double toSec (double hour, double min, double sec) {
 int main() {
     int choice;
     double a,b,c,r,h,hour,min,sec;
-    std::cout << "1 - Find triangle area by height and side"
-                 "\n2 - Find triangle area by three sides"
-                 "\n3 - Find rectangle area"
-                 "\n4 - Find circle area"
-                 "\n5 - Find factorial"
-                 "\n6 - Find minimal of two numbers"
-                 "\n7 - hh:mm:ss to sec" << std::endl;
-    std::cin >> choice;
+    while (1) {
+        std::cout << "1 - Find triangle area by height and side"
+                     "\n2 - Find triangle area by three sides"
+                     "\n3 - Find rectangle area"
+                     "\n4 - Find circle area"
+                     "\n5 - Find factorial"
+                     "\n6 - Find minimal of two numbers"
+                     "\n7 - hh:mm:ss to sec" << std::endl;
+        std::cin >> choice;
 
-    switch (choice) {
-        default:
-            std::cout << "Unknown operation, restart program" << std::endl;
-            break;
-        case 1:
-            std::cout << "Insert height" << std::endl;
-            std::cin >> h;
+        switch (choice) {
+            default:
+                std::cout << "Unknown operation, restart program" << std::endl;
+                break;
+            case 1:
+                std::cout << "Insert height" << std::endl;
+                std::cin >> h;
 
-            std::cout << "Insert side" << std::endl;
-            std::cin >> a;
+                std::cout << "Insert side" << std::endl;
+                std::cin >> a;
 
-            std::cout << "Triangle area: " << triangleHeight(a,h) << std::endl;
-            break;
-        case 2:
-            std::cout << "Insert first side" << std::endl;
-            std::cin >> a;
+                std::cout << "Triangle area: " << triangleHeight(a,h) << std::endl;
+                break;
+            case 2:
+                std::cout << "Insert first side" << std::endl;
+                std::cin >> a;
 
-            std::cout << "Insert second side" << std::endl;
-            std::cin >> b;
+                std::cout << "Insert second side" << std::endl;
+                std::cin >> b;
 
-            std::cout << "Insert third side" << std::endl;
-            std::cin >> c;
+                std::cout << "Insert third side" << std::endl;
+                std::cin >> c;
 
-            std::cout << "Triangle area: " << triangleHeron(a,b,c) << std::endl;
-            break;
-        case 3:
-            std::cout << "Insert first side" << std::endl;
-            std::cin >> a;
+                std::cout << "Triangle area: " << triangleHeron(a,b,c) << std::endl;
+                break;
+            case 3:
+                std::cout << "Insert first side" << std::endl;
+                std::cin >> a;
 
-            std::cout << "Insert second side" << std::endl;
-            std::cin >> b;
+                std::cout << "Insert second side" << std::endl;
+                std::cin >> b;
 
-            std::cout << "Rectangle area: " << rectangleArea(a,b) << std::endl;
-            break;
-        case 4:
-            std::cout << "Insert radius" << std::endl;
-            std::cin >> r;
+                std::cout << "Rectangle area: " << rectangleArea(a,b) << std::endl;
+                break;
+            case 4:
+                std::cout << "Insert radius" << std::endl;
+                std::cin >> r;
 
-            std::cout << "Circle area: " << circleArea(r) << std::endl;
-            break;
-        case 5:
-            std::cout << "Insert int number" << std::endl;
-            std::cin >> a;
+                std::cout << "Circle area: " << circleArea(r) << std::endl;
+                break;
+            case 5:
+                std::cout << "Insert int number" << std::endl;
+                std::cin >> a;
 
-            std::cout << "Factorial of number: " << factorial(a) << std::endl;
-            break;
-        case 6:
-            std::cout << "Insert first number" << std::endl;
-            std::cin >> a;
+                std::cout << "Factorial of number: " << factorial(a) << std::endl;
+                break;
+            case 6:
+                std::cout << "Insert first number" << std::endl;
+                std::cin >> a;
 
-            std::cout << "Insert second number" << std::endl;
-            std::cin >> b;
+                std::cout << "Insert second number" << std::endl;
+                std::cin >> b;
 
-            std::cout << "Factorial of number: " << findMin(a,b) << std::endl;
-        case 7:
-            std::cout << "Insert hours" << std::endl;
-            std::cin >> hour;
+                std::cout << "Factorial of number: " << findMin(a,b) << std::endl;
+            case 7:
+                std::cout << "Insert hours" << std::endl;
+                std::cin >> hour;
 
-            std::cout << "Insert minutes" << std::endl;
-            std::cin >> min;
+                std::cout << "Insert minutes" << std::endl;
+                std::cin >> min;
 
-            std::cout << "Insert seconds" << std::endl;
-            std::cin >> sec;
+                std::cout << "Insert seconds" << std::endl;
+                std::cin >> sec;
 
-            std::cout << "Your time in seconds: " << toSec(hour,min,sec) << std::endl;
+                std::cout << "Your time in seconds: " << toSec(hour,min,sec) << std::endl;
+        }
     }
 }
